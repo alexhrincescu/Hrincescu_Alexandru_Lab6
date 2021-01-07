e96a296a28fe986f463e1358468f21673c085f21
Hrincescu Alexandru <alexhrincescu@gmail.com>
2021-01-07 17:04:32 +0200

    Add project files.

---
+++ Data/Hrincescu_Alexandru_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hrincescu_Alexandru_Lab6.Models;
+
+namespace Hrincescu_Alexandru_Lab6.Data
+{
+    public class Hrincescu_Alexandru_Lab6Context : DbContext
+    {
+        public Hrincescu_Alexandru_Lab6Context (DbContextOptions<Hrincescu_Alexandru_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hrincescu_Alexandru_Lab6.Models.Book> Book { get; set; }
+    }
+}
